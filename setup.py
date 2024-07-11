1734c219e7cd9430498c227c85ad86111ef6cc6b
toooommm <106803549+Gelbbrustara@users.noreply.github.com>
2024-07-11 22:59:00 +0200

    Beta V1.2
    V1.2 -added support

--- setup.py
+++ setup.py
@@ -3,15 +3,21 @@ import setuptools
 
 setuptools.setup(
     name="FilamentManager",
-    version="1.0",
+    version="1.1",
     url="https://github.com/Gelbbrustara/octo_filament_manager_V2",
     author="Gelbbrustara",
     author_email="peterotzi79@gmail.com",
     description="OctoPrint plugin for managing prints and filament usage.",
-    packages=setuptools.find_packages(),
+packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=[
         "OctoPrint>=1.3.12",
     ],
+    entry_points={
+        "octoprint.plugin": [
+            "filament_manager = filament_manager.plugin"
+        ]
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
--- static/js/filament_manager.js
+++ static/js/filament_manager.js
@@ -1,4 +1,44 @@
 // filament_manager/static/js/filament_manager.js
 $(function() {
-    // Hier können jQuery oder plain JavaScript Funktionen hinzugefügt werden
+    function updateFilamentList(filamentData) {
+        var filamentList = $("#filament_list");
+        filamentList.empty();
+        filamentData.forEach(function(filament) {
+            var filamentItem = $("<div>").addClass("filament-item");
+            var filamentColor = $("<div>").addClass("filament-color").css("background-color", filament.color);
+            var filamentName = $("<div>").addClass("filament-name").text(filament.name);
+            var filamentBar = $("<div>").addClass("filament-bar").css("width", filament.usage + "%");
+            filamentItem.append(filamentColor, filamentName, filamentBar);
+            filamentList.append(filamentItem);
+        });
+    }
+
+    function fetchFilamentData() {
+        $.get("/plugin/filament_manager/get_filament_data", function(response) {
+            updateFilamentList(response.filament_data);
+        });
+    }
+
+    $("#add_filament_form").submit(function(event) {
+        event.preventDefault();
+        var formData = {
+            name: $("#filament_name").val(),
+            color: $("#filament_color").val(),
+            usage: $("#filament_usage").val()
+        };
+        $.ajax({
+            url: "/plugin/filament_manager/add_filament",
+            type: "POST",
+            contentType: "application/json",
+            data: JSON.stringify(formData),
+            success: function(response) {
+                if (response.success) {
+                    fetchFilamentData();
+                    $("#add_filament_form")[0].reset();
+                }
+            }
+        });
+    });
+
+    fetchFilamentData();
 });
